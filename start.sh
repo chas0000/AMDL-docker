1b856dc3b40b620b04b0df1ae90a79f1e1881244
chas0000 <86915826+chas0000@users.noreply.github.com>
2025-11-09 10:02:18 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,13 @@ echo "尝试启动"
 # 1. 检查 /app/amdl/config.yaml 是否存在
 if [ ! -f /app/config/config.yaml ]; then
     cp /app/backup/config.yaml /app/config.yaml
+    cp /app/backup/config.yaml /app/config/config.yaml
 else
     cp /app/config/config.yaml /app/config.yaml   
 fi
 if [ ! -f /app/config/sky_config.yaml ]; then
     cp /app/backup/sky_config.yaml /app/sky_config.yaml
+    cp /app/backup/sky_config.yaml /app/config/sky_config.yaml
 else
     cp /app/config/sky_config.yaml /app/sky_config.yaml  
 fi
