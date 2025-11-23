c9f41e788c6faba76d78d2ad2b1398a6d24775bc
chas0000 <86915826+chas0000@users.noreply.github.com>
2025-11-23 21:44:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -35,9 +35,16 @@ SESSION_NAME=mysession
 # -----------------------------
 #  启动 tmux session 后台运行 Python（只启动一次）
 # -----------------------------
-if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
+if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
     echo "[INFO] Creating tmux session $SESSION_NAME..."
-    tmux new-session -d -s $SESSION_NAME "bash"
+
+    if [ -f /app/config/.tmux.conf ]; then
+        # 配置文件存在，加载自定义 tmux 配置
+        tmux new-session -d -s "$SESSION_NAME" -f /app/config/.tmux.conf "bash"
+    else
+        # 配置文件不存在，使用默认配置启动
+        tmux new-session -d -s "$SESSION_NAME" "bash"
+    fi
 fi
 
 # -----------------------------
